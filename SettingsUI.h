f3053598fff0a8cad59d6b7c8523bcf9a8469fdf
look4green <look4green@users.noreply.github.com>
2025-07-23 02:06:26 +0800

    Create SettingsUI.h

---
+++ SettingsUI.h
@@ -0,0 +1,9 @@
+#ifndef SETTINGS_UI_H
+#define SETTINGS_UI_H
+
+#include <Arduino.h>
+
+void initSettingsPanel();       // Load settings screen
+void updateSettingsPanel();     // Handle input and redraw UI
+
+#endif
