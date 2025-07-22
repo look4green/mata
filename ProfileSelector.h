2f325397dc4510a49b52d76cd581eb623f265bc1
look4green <look4green@users.noreply.github.com>
2025-07-23 02:02:13 +0800

    Create ProfileSelector.h

---
+++ ProfileSelector.h
@@ -0,0 +1,9 @@
+#ifndef PROFILE_SELECTOR_H
+#define PROFILE_SELECTOR_H
+
+#include <Arduino.h>
+
+void initProfileSelector();       // Set up profile scan
+void updateProfileSelector();     // Refresh display and handle input
+
+#endif
